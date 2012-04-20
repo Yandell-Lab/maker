45ada08d4440d5d34a5ecc7de397733992dd0f18
cholt <cholt@e9ca6701-0a3e-0410-8439-dc1246d35178>
2012-04-20 21:53:07 +0000

    disable ProcessTable replacement

--- lib/Proc/ProcessTable_simple.pm
+++ lib/Proc/ProcessTable_simple.pm
@@ -9,7 +9,7 @@ use Carp;
 use base qw();
 
 our $VERSION='1.0';
-our $PS = File::Which::which('ps');
+our $PS;# = File::Which::which('ps');
 
 sub new {
     if($PS){
@@ -31,7 +31,7 @@ sub table {
 	      " -o ppid=ppid".("_"x10).
 	      " -o state=state".("_"x10).
 	      " -o comm=fname".("_"x500).
-	      " -o args=cmndline -w -w |";
+	      " -o args=cmndline -w -w 2> /dev/null |";
 
     open(PS, $cmd);
     my $i = 0;
