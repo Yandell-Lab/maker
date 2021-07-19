1de7d11e55b76de339653a27db8056c379dd3e08
carsonhh <carsonhh@gmail.com>
2021-07-19 13:29:30 -0600

    Revert "Update evaluator"
    This reverts commit 3590ba86b77aa485affd5c7002fe1250d6a6878e.

--- src/inc/lib/MAKER/Build.pm
+++ src/inc/lib/MAKER/Build.pm
@@ -445,8 +445,8 @@ sub ACTION_release {
     if(! -f $tgz){
 	my ($dir, $base) = $cwd =~ /^(.*\/)([^\/]+)\/src$/;
 	
-	my $exclude = `cd $dir/$base; git status -s`;
-	$exclude = join("\n", ($exclude =~ /\?\?\s+([^\n]+)/g), "maker/src/maker-$ver.tgz") ."\n";
+	my $exclude = `cd $dir; git status $base`;
+	$exclude = join("\n", ($exclude =~ /\?\s+([^\n]+)/g), "maker/src/maker-$ver.tgz") ."\n";
 	open(OUT, "> .exclude~");
 	print OUT $exclude;
 	close(OUT);
